9086e400eed7bd570f8888c19f1d294428400a24
Loray21 <loray98@gmail.com>
2022-10-08 12:13:24 -0300

    add url admin

--- entregafinalcoder/entregafinal/entregafinal/urls.py
+++ entregafinalcoder/entregafinal/entregafinal/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path("", include("AppEcommerce.urls")),
     path("", include("AppMensajeria.urls"))
 ]
